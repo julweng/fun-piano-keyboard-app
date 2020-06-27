aa7183c2e0788d554f5492a6c5cae5b0a9ea3ade
julweng <julweng@gmail.com>
2020-06-26 23:46:06 -0700

    app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,21 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import App from "./App"
+
+jest.mock("./components/Octave/Octave.tsx", () =>
+  jest.fn(() => <div>Octave</div>)
+)
 
 describe("App", () => {
   it("Should render App", () => {
     const { container } = render(<App />)
     expect(container).toMatchSnapshot()
+  })
+
+  it("Should render Octave", () => {
+    render(<App />)
+    const Octave = screen.getByText("Octave")
+    expect(Octave).toBeTruthy()
   })
 })
 
